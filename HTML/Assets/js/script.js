bee130f15bc0020eb9518dcb24f4c05563fb6e76
Aliacf21 <51883210+Aliacf21@users.noreply.github.com>
2020-12-15 09:24:22 -0500

    1 commit

---
+++ HTML/Assets/js/script.js
@@ -0,0 +1,52 @@
+function showCodingField2() {
+	$("#step2").show();
+}
+
+function showCodingField341() {
+	$("#step3Line4Part1").show();
+}
+
+function showCodingField342() {
+	$("#step3Line4Part2").show();
+}
+
+function showCodingField351() {
+	$("#step3Line5Part1").show();
+}
+
+function showCodingField352() {
+	$("#step3Line5Part2").show();
+}
+
+function showCodingField361() {
+	$("#step3Line6Part1").show();
+}
+
+function showCodingField362() {
+	$("#step3Line6Part2").show();
+}
+
+function showCodingField371() {
+	$("#step3Line7Part1").show();
+}
+
+function showCodingField372() {
+	$("#step3Line7Part2").show();
+}
+
+function showCodingField4() {
+	$("#step4").show();
+}
+
+function showCodingField5() {
+	$("#step5").show();
+}
+
+function showCodingField6() {
+	$("#step6").show();
+}
+
+
+
+
+
